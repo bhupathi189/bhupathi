e3295711e0d72328df8da82432c0d4e2134b44ae
bhupathi189 <35564316+bhupathi189@users.noreply.github.com>
2020-08-07 15:43:48 +0530

    Update projects.jsx

--- src/Components/projects.jsx
+++ src/Components/projects.jsx
@@ -1,1 +1,23 @@
-import react from 'react';
+import React, {Component} from 'react';
+
+class Projects extends Component {
+  state ={};
+  render(){
+    return (
+      <div className="content-wrap">
+        <div className=flex-container>
+          
+        </div>
+       </div>
+    )
+  }
+  project(var imgurl, var project name) {
+    return(
+    <div className="flex-container">
+        <img src={this.imgurl} alt=""project related image">
+           <span>{this.projectname}</span>
+        </img>
+    </div>
+    )
+  }
+}
